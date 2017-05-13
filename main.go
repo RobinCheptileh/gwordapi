99d53b2ae73d85c1ab7accbc3d2ccbb8bc40659f
Robin Cheptileh <robincheptileh@gmail.com>
2017-05-13 21:26:56 +0300

    First real Gword deploy

--- main.go
+++ main.go
@@ -2,27 +2,175 @@ package main
 
 import (
 	"net/http"
-	"math/rand"
+	"time"
+	"fmt"
+	"strings"
+	"sync"
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
 )
 
 var router *gin.Engine
 
+var wsupgrader = websocket.Upgrader{
+	ReadBufferSize: 1024,
+	WriteBufferSize: 1024,
+}
+
+type request struct {
+	Letters string
+	Limit int
+	Stop bool
+}
+
+type response struct {
+	Word string
+}
+
+type done struct {
+	Done bool
+}
+
+func wordGenerator(conn *websocket.Conn, message request, stop chan bool, wg *sync.WaitGroup)  {
+	defer wg.Done()
+	defer track(time.Now(), "wordGenerator()")
+
+	var temp_word string
+	var temp_list []string
+
+	fmt.Println(getPermCount(message.Letters, message.Limit))
+	for i := 0; i < getPermCount(message.Letters, message.Limit); i++ {
+
+		select {
+		default:
+			temp_word = getPerm(message.Letters, message.Limit, i)
+			if isInDictionary(temp_word){
+				found := false
+				for _, v := range temp_list{
+					if v == temp_word{
+						found = true
+					}
+				}
+				if !found{
+					temp_list = append(temp_list, temp_word)
+					fmt.Println(temp_word)
+					word := response{temp_word}
+					err := conn.WriteJSON(word)
+					if err != nil {
+						return
+					}
+				}
+			}
+
+		case <- stop:
+			fmt.Println("Stopped")
+			don := done{true}
+			err :=  conn.WriteJSON(don)
+			if err != nil {
+				return
+			}
+			return
+		}
+	}
+
+	fmt.Println("Done!")
+	don := done{true}
+	err :=  conn.WriteJSON(don)
+	if err != nil {
+		return
+	}
+}
+
+func wshandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := wsupgrader.Upgrade(w, r, nil)
+	checkErr(err)
+	fmt.Println("Websocket Initiated")
+	defer conn.Close()
+
+	var message request
+	stop := make(chan bool)
+	wg := &sync.WaitGroup{}
+
+	for {
+
+		err = conn.ReadJSON(&message)
+		checkErr(err)
+		fmt.Println(message)
+
+		if message.Stop{
+			fmt.Println("Trying to stop")
+			stop <- true
+			wg.Wait()
+		}else{
+			message.Letters = strings.ToLower(message.Letters)
+			fmt.Println(message.Letters)
+			fmt.Println(message.Limit)
+
+			go wordGenerator(conn, message, stop, wg)
+			wg.Add(1)
+		}
+	}
+}
+
+func getPermCount(letters string, count int) (int) {
+	result := 1
+	//k characters from a set of n has n!/(n-k)! possible combinations
+	for i := len(letters) - count + 1; i <= len(letters); i++ {
+		result *= i;
+	}
+	return result
+}
+
+func getPerm(letters string, count int, index int) (string){
+	result := ""
+	//Decodes index to a $count-length string from $letters, no repeat chars.
+	i := 0
+	for i < count {
+		pos := index % len(letters)
+		result += string(letters[int(pos)])
+		index = (index - pos) / len(letters)
+		a := int(pos + 1)
+		b := int(len(letters))
+		letters = letters[0:int(pos)] + letters[a:b]
+		i = i + 1
+	}
+	return result
+}
+
+func track(start time.Time, name string) {
+	elapsed := time.Since(start)
+	fmt.Println()
+	fmt.Printf("[TRACKER] : %s took %s", name, elapsed.String())
+	fmt.Println()
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
 func main() {
 
+	gin.SetMode(gin.ReleaseMode)
 	// Set the router as the default one provided by Gin
 	router = gin.Default()
 
+	router.Static("/css", "templates/css")
+	router.Static("/fonts", "templates/fonts")
+	router.Static("/js", "templates/js")
+	router.Static("/img", "templates/img")
+
 	// Process the templates at the start so that they don't have to be loaded
 	// from the disk again. This makes serving HTML pages very fast.
-	router.LoadHTMLGlob("templates/*")
+	router.LoadHTMLGlob("templates/index.html")
 
 	// Define the route for the index page and display the index.html template
 	// To start with, we'll use an inline route handler. Later on, we'll create
 	// standalone functions that will be used as route handlers.
 	router.GET("/", func(c *gin.Context) {
-		names := []string{"Robin", "Perez", "Robie", "Robyn", "Cheptileh", "Adhiambo", "Pretty"}
-		name := names[rand.Intn(len(names))]
+		//names := []string{"Robin", "Perez", "Robie", "Robyn", "Cheptileh", "Adhiambo", "Pretty"}
+		//name := names[rand.Intn(len(names))]
 		// Call the HTML method of the Context to render a template
 		c.HTML(
 			// Set the HTTP status to 200 (OK)
@@ -31,15 +179,18 @@ func main() {
 			"index.html",
 			// Pass the data that the page uses (in this case, 'title')
 			gin.H{
-				"title": "Home Page",
-				"name": name,
-				"content": "This will be some major content folks!\nStaytuned",
+				"name" : "Robin Cheptileh",
+				"year" : time.Now().Year(),
 			},
 		)
 
 	})
 
+	router.GET("/ws", func(c *gin.Context) {
+		wshandler(c.Writer, c.Request)
+	})
+
 	// Start serving the application
-	router.Run()
+	router.Run(":5000")
 
 }
---
+++ templates/js/app.js
@@ -0,0 +1,285 @@
+$(document).foundation()
+
+var ws;
+var url = "ws://localhost:5000/ws";
+//var url = "ws://192.168.137.1:5000/ws";
+var enabled = false;
+var stop = false;
+var inProgress = false;
+var scrolled = false;
+var initialScroll = 0;
+var $theForm = $("#the-form");
+var $letters = $("#letters");
+var $limit = $("#limit");
+var $generate = $(".generate");
+var $response = $("#response");
+var $temp_stop = $("#temp-stop");
+var $footer = $("#footer");
+
+$(document).ready(function () {
+    if(window.WebSocket === undefined){
+        var notification = $("<div>")
+            .attr({
+                class : "notification"
+            })
+            .html("<i class='fa fa-exclamation-circle'></i>&nbsp; dangit! your browser doesn't support websockets :(")
+            .hide()
+            .velocity("transition.slideDownIn", {
+                duration : 200,
+                easing : "ease-in-out"
+            })
+            .velocity("transition.slideUpOut", {
+                delay : 2000,
+                duration : 200,
+                easing : "ease-in-out"
+            });
+        $("body").append(notification);
+        console.log("WebSocket unsupported");
+    }else{
+        ws = initWebSocket();
+        console.log("WebSocket initiated");
+        enabled = true;
+    }
+    if($(window).width() <= (39.9375 * 16)){
+        $footer.children().eq(0).removeClass("small-4");
+        $footer.children().eq(0).addClass("small-12");
+        $footer.children().eq(2).css({
+            display : "none"
+        });
+        $footer.children().eq(1).css({
+            display : "none"
+        });
+    }
+});
+
+$(window).resize(function () {
+    if($(window).width() <= (39.9375 * 16)){
+        if($footer.children().length > 1){
+            $footer.children().eq(0).removeClass("small-4");
+            $footer.children().eq(0).addClass("small-12");
+            $footer.children().eq(2).css({
+                display : "none"
+            });
+            $footer.children().eq(1).css({
+                display : "none"
+            });
+        }
+    }else{
+        $footer.children().eq(0).removeClass("small-12");
+        $footer.children().eq(0).addClass("small-4");
+        $footer.children().eq(2).css({
+            display : "flex"
+        });
+        $footer.children().eq(1).css({
+            display : "flex"
+        });
+    }
+});
+
+$(window).scroll(function () {
+    var currentScroll = $(this).scrollTop();
+    if (inProgress){
+        if (!scrolled){
+            if (currentScroll < initialScroll){
+                //Scrolling Up
+                scrolled = true;
+                var notification = $("<div>")
+                    .attr({
+                        class : "notification"
+                    })
+                    .html("<i class='fa fa-info-circle'></i>&nbsp; auto-scroll disabled")
+                    .hide()
+                    .velocity("transition.slideDownIn", {
+                        duration : 200,
+                        easing : "ease-in-out"
+                    })
+                    .velocity("transition.slideUpOut", {
+                        delay : 2000,
+                        duration : 200,
+                        easing : "ease-in-out"
+                    });
+                $("body").append(notification);
+            }
+        }
+    }
+    initialScroll = currentScroll;
+});
+
+function initWebSocket() {
+    var socket = new WebSocket(url);
+    var count = 1;
+    var scroll_count = 1;
+
+    socket.onopen = function () {
+        console.log("WebSocket ready");
+    };
+
+    socket.onmessage = function (e) {
+        var res = JSON.parse(e.data);
+        if(res['Done'] === true){
+            var word_item = $("<div>")
+                .attr({
+                    class : "word-container small-6 medium-4 large-4 column"
+                })
+                .html("")
+                .hide()
+                .velocity("fadeIn", {
+                    duration : 200,
+                    easing : "ease-in-out"
+                });
+            if(stop){
+                var notification = $("<div>")
+                    .attr({
+                        class : "notification"
+                    })
+                    .html("<i class='fa fa-exclamation-circle'></i>&nbsp; Stopped")
+                    .hide()
+                    .velocity("transition.slideDownIn", {
+                        duration : 200,
+                        easing : "ease-in-out"
+                    })
+                    .velocity("transition.slideUpOut", {
+                        delay : 2000,
+                        duration : 200,
+                        easing : "ease-in-out"
+                    });
+            }else{
+                notification = $("<div>")
+                    .attr({
+                        class : "notification"
+                    })
+                    .html("<i class='fa fa-check-circle'></i>&nbsp; Done!")
+                    .hide()
+                    .velocity("transition.slideDownIn", {
+                        duration : 200,
+                        easing : "ease-in-out"
+                    })
+                    .velocity("transition.slideUpOut", {
+                        delay : 2000,
+                        duration : 200,
+                        easing : "ease-in-out"
+                    });
+            }
+
+            $response.append(word_item);
+            $("body").append(notification);
+
+            $letters.attr({
+                disabled : false
+            });
+            $limit.attr({
+                disabled : false
+            });
+            $generate.removeClass("stop");
+            $generate.addClass("generate");
+            $generate.html("<i class='fa fa-cogs'></i>&nbsp;generate!");
+            $temp_stop.children().remove();
+
+            count = 1;
+            scroll_count = 1;
+            scrolled = false;
+            inProgress = false;
+        }else{
+            word_item = $("<div>")
+                .attr({
+                    class : "word-container small-6 medium-4 large-4 column"
+                })
+                .html(
+                    "<div class='word-item small-12 column'>" +
+                        count + ". " + res['Word'] +
+                    "</div>"
+                )
+                .hide()
+                .velocity("fadeIn", {
+                    duration : 200,
+                    easing : "ease-in-out"
+                });
+            $response.append(word_item);
+            count++;
+            if(($response.height() + $response.offset().top) >= $(window).height()){
+                if(scroll_count <= 15) {
+                    scroll_count++;
+                }else{
+                    if (!scrolled){
+                        $response.velocity("scroll", {
+                            duration: 500,
+                            offset: $response.height() - 20,
+                            easing: "ease-in-out"
+                        });
+                        scroll_count = 1;
+                    }
+                }
+            }
+        }
+    };
+
+    socket.onclose = function () {
+        $generate.removeClass("stop");
+        $generate.addClass("generate");
+        $generate.html("<i class='fa fa-cogs'></i>&nbsp;generate!");
+        $temp_stop.children().remove();
+
+        inProgress = false;
+        console.log("WebSocket closed");
+    };
+
+    return socket;
+}
+
+$theForm.submit(function (e) {
+   e.preventDefault();
+
+   if(enabled) {
+       if (inProgress) {
+           stop = true;
+           ws.send(JSON.stringify({
+               Letters: "stop",
+               Limit: 0,
+               Stop: true
+           }));
+       } else {
+           stop = false;
+           $response.contents().velocity("fadeOut", {
+               duration: 200,
+               easing: "ease-in-out"
+           });
+           $response.html("");
+
+           $letters.attr({
+               disabled: true
+           });
+           $limit.attr({
+               disabled: true
+           });
+           $generate.removeClass("generate");
+           $generate.addClass("stop");
+           $generate.html("<i class='fa fa-stop-circle'></i>&nbsp;stop");
+           $generate.clone().appendTo("#temp-stop");
+
+           ws.send(JSON.stringify({
+               Letters: $("#letters").val(),
+               Limit: parseInt($("#limit").val()),
+               Stop: false
+           }));
+
+           inProgress = true;
+       }
+   }else{
+       var notification = $("<div>")
+           .attr({
+               class : "notification"
+           })
+           .html("<i class='fa fa-exclamation-circle'></i>&nbsp; dangit! your browser doesn't support websockets :(")
+           .hide()
+           .velocity("transition.slideUpIn", {
+               duration : 200,
+               easing : "ease-in-out"
+           })
+           .velocity("transition.slideDownOut", {
+               delay : 2000,
+               duration : 200,
+               easing : "ease-in-out"
+           });
+       console.log("WebSocket unsupported");
+   }
+});
